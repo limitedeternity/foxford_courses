24153994357db268476dbc57781c75235b7fbf25
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-08-16 10:02:53 +0300

    Delete __init__.py

--- foxford_downloader/modules/operations/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ElementScreenshot import *
-from .SortFiles import *
-from .TheoryHTML import *
-from .Downloader import *
-from .VideoHTML import *
-from .OperatorShifted import *
-from .Operator import *
