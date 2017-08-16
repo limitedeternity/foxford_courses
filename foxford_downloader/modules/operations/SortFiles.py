9f83f8acdc64343af60d2dcda3e8a1932f66e7b5
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-08-16 12:53:12 +0300

    Add files via upload

--- foxford_downloader/modules/operations/SortFiles.py
+++ foxford_downloader/modules/operations/SortFiles.py
@@ -1,27 +1,24 @@
-from glob import glob
-from os import unlink
+from os import unlink, listdir
 from io import open as ioopen
 from os.path import join, abspath
 from shutil import move
 
 
 def sort_files(course_name):
-    for filename in glob(join(abspath('.'), '*.mp4')):
-        if filename is not None:
+    for filename in listdir(abspath(".")):
+        if filename.endswith(".mp4"):
             move(
                 join(abspath('.'), filename),
                 join(abspath('.'), course_name, filename.strip('.mp4'), filename)
             )
 
-    for filename in glob(join(abspath('.'), '*.png')):
-        if filename is not None:
+        elif filename.endswith(".png"):
             move(
                 join(abspath('.'), filename),
                 join(abspath('.'), course_name, "Теория", filename)
             )
 
-    for filename in glob(join(abspath('.'), '*.html')):
-        if filename is not None:
+        elif filename.endswith(".html"):
             unlink(join(abspath('.'), filename))
 
     with ioopen(join(abspath('.'), 'links.txt'), "w") as ph:
