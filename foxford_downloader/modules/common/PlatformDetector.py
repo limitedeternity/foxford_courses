e18f463d3d7126a85d83b1babada198fa3734c9b
Marise Hayashi <limitedeternity@users.noreply.github.com>
2017-10-08 23:13:33 +0300

    Update PlatformDetector.py

--- foxford_downloader/modules/common/PlatformDetector.py
+++ foxford_downloader/modules/common/PlatformDetector.py
@@ -8,14 +8,8 @@ def system_platform():
 
     if platform.startswith('win'):
         return 'modules/driver/win/chromedriver.exe'
-    elif platform.startswith('darwin'):
-        return 'modules/driver/darwin/chromedriver'
-    elif platform.startswith('linux'):
-        if machine().endswith('64'):
-            return 'modules/driver/linux/x64/chromedriver'
-        else:
-            return 'modules/driver/linux/x86/chromedriver'
+
     else:
-        print("Ошибка идентификации системы.")
+        print("Not Windows.")
         sleep(1)
         exit(0)
